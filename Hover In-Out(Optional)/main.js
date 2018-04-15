697d18cc20e1939ca098b8da18c3d07ae74d99a0
Biniam <biniam22@gmail.com>
2018-04-14 22:33:26 -0400

    Hover In/Out project

--- Hover In-Out(Optional)/main.js
+++ Hover In-Out(Optional)/main.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-    var src = "img/";
-    var rndNum = function(){return Math.floor(Math.random()*4+1);}
-    var rndImg = function(){return src+"ninja"+rndNum()+".png";}
 
-    $('body img').hover(function(){
-        $(this).attr("src",rndImg());
-    },function(){
-        var orig = $(this).attr("altSrc");
-        $(this).attr("src",orig);
-    });
-});
+ $(document).ready(function(){
+            $('img').hover(
+                function(){
+                    $(this).attr('temp', $(this).attr('src'));
+                    $(this).attr('src', $(this).attr('data-alt-src'));
+                    $(this).attr('data-alt-src', $(this).attr('temp'));
+                },
+                function(){
+                    $(this).attr('temp', $(this).attr('src'));
+                    $(this).attr('src', $(this).attr('data-alt-src'));
+                    $(this).attr('data-alt-src', $(this).attr('temp'));
+            });
+        });
